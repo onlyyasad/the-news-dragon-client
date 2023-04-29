092508ef74de4cb5fd6e116c0487574068187b93
onlyyasad <onlyyasad@gmail.com>
2023-04-29 16:27:18 +0600

    Left NavBar data Loaded and Visualized in UI

--- src/pages/Shared/LeftNav.jsx
+++ src/pages/Shared/LeftNav.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/categories')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+        .catch(error => console.error(error))
+    }, [])
+
+    
     return (
         <div>
-            <p>This is leftside Navigation</p>
+            <h4>All Category</h4>
+            <div className='ps-4'>
+                {
+                    categories.map(category => <Button className='w-100 text-start mb-2' variant="light" key={category.id}>
+                        <Link className='text-decoration-none text-black' to={`/category/${category.id}`}>{category.name}</Link>
+                    </Button> )
+                }
+            </div>
         </div>
     );
 };
